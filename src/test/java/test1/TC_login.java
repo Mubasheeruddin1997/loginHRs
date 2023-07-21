bca76e607a09648e810bd419dfe165a1111d8c9f
ADMIN <ADMIN@DESKTOP-C1M01HP>
2023-07-21 08:06:05 +0530

    first commit

---
+++ src/test/java/test1/TC_login.java
@@ -0,0 +1,36 @@
+package test1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC_login {
+	
+	@Test
+	public void login_Test() throws InterruptedException {
+		
+			WebDriverManager.chromedriver().setup();
+			WebDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			
+			driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+			
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+			driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+			driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+			driver.findElement(By.xpath("//button[@type='submit']")).click();
+			
+			Assert.assertEquals(driver.getTitle(),"OrangeHRM");
+			driver.close();
+			
+		}
+		
+	}
+
